3e84e0cf9f344990e53569965af6cc2d0df33f67
Alvaro Israel <israel.nunes.leite@gmail.com>
2020-04-20 19:16:22 -0300

    chore: Implemented business logic to delete a transaction.

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,18 @@
-// import AppError from '../errors/AppError';
+import {getCustomRepository} from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transactions does not exist.');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
